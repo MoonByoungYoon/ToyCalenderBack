ca96769092ed96d6b44f2c717c386d46a22c43b3
Byung Yoon Moon <mby0225@naver.com>
2024-09-04 17:07:58 +0900

    cors 설정

---
+++ src/main/java/com/toy/config/CorsFilter.java
@@ -0,0 +1,38 @@
+package com.toy.config;
+
+import java.io.IOException; 
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CorsFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "*");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			filterChain.doFilter(req, res);
+		}
+	}
+
+}
--- src/main/java/com/toy/config/WebConfig.java
+++
@@ -1,5 +0,0 @@
-package com.toy.config;
-
-public class WebConfig {
-
-}
